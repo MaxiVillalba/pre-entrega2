1a4e051db5c3a08f4ed3c6ad80aaf227c27a00ca
unknown <massevillalba@gmail.com>
2024-12-11 17:48:21 +0100

    se realizaron cambios de estilo con boostrap

--- public/js/index.js
+++ public/js/index.js
@@ -1,68 +1,57 @@
-// Configuramos el socket del lado del cliente
-const socket = io(); // Hacemos referencia a socket.io
-
+const socket = io();
 let user;
-
 let chatBox = document.getElementById("chatBox");
 
-// Alerta para ingresar datos
-
 Swal.fire({
-title: "Identificate",
-input: "text",
-text: "Ingresá el usuario para identificarte",
-inputValidator: (value) => {
-  return !value && "Por favor, ingresé su nombre de usuario"
-},
-allowOutsideClick: false
-}).then ((result) => {
+  title: "Identificate",
+  input: "text",
+  text: "Ingresá el usuario para identificarte",
+  inputValidator: (value) => {
+    return !value && "Por favor, ingresé su nombre de usuario";
+  },
+  allowOutsideClick: false,
+}).then((result) => {
   user = result.value;
-  console.log(user);
-
-  //Se envía el user al server
-
   socket.emit("newUser", user);
-})
+});
 
 chatBox.addEventListener("keyup", (event) => {
-  if(event.key === "Enter") {
- if(chatBox.value.trim().length > 0) {
-  socket.emit("message", { user: user, message: chatBox.value })
-  chatBox.value = "";
- }
+  if (event.key === "Enter") {
+    if (chatBox.value.trim().length > 0) {
+      socket.emit("message", { user: user, message: chatBox.value });
+      chatBox.value = "";
+    }
   }
-})
-
-// recibir los mensajes del chat actualizados
+});
 
 socket.on("messageLogs", (data) => {
   let messagesLogs = document.getElementById("messageLogs");
   let messages = "";
 
   data.forEach((messageLog) => {
-    messages = messages + `${messageLog.user} dice: ${messageLog.message}</br>`
+    const messageClass = messageLog.user === user ? "text-end" : "text-start";
+    messages += `<div class="${messageClass}"><strong>${messageLog.user}</strong>: ${messageLog.message}</div>`;
   });
 
-  messagesLogs.innerHTML= messages;
-})
+  messagesLogs.innerHTML = messages;
+});
 
 socket.on("newUser", (data) => {
   Swal.fire({
-    text:`se conecto ${data}`,
+    text: `Se conectó ${data}`,
     toast: true,
     position: "top-right",
     timer: 2000,
+    showConfirmButton: false,
   });
-
-
-    })
-
+});
 
 socket.on("user-disconnect", (data) => {
-    swal.fire({
-      text: `${data} se desconectó`,
-      toast: true,
-      position: "top-right",
-      timer: 2000,
-      showConfirmButton: false,  })
+  Swal.fire({
+    text: `${data} se desconectó`,
+    toast: true,
+    position: "top-right",
+    timer: 2000,
+    showConfirmButton: false,
+  });
 });
--- src/app.js
+++ src/app.js
@@ -4,21 +4,20 @@ import { Server } from "socket.io";
 import fs from "fs";
 
 const app = express();
-// Para que nuestro servidor express pueda interpretar en forma automática mensajes de tipo JSON
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const PORT = 8080;
-// Configuración de handlebars
+
 app.engine("handlebars", handlebars.engine());
 app.set("views", "./src/views");
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  
   res.render("index");
-})
+});
 
 const httpServer = app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
@@ -26,57 +25,52 @@ const httpServer = app.listen(PORT, () => {
 
 let messages = [];
 
-// Configuración para guardar mensajes
-
 const saveMessages = async (messages) => {
   try {
-  await fs.promises.writeFile('./messages.json', JSON.stringify(messages, null, 2));
-} catch (error) {
-  console.error('Error al intentar guardar los mensajes', error);
-}};
-
-// Cargar mensajes previos y generar messages.json
+    await fs.promises.writeFile('./messages.json', JSON.stringify(messages, null, 2));
+  } catch (error) {
+    console.error('Error al intentar guardar los mensajes', error);
+  }
+};
 
 const loadMessageFromFile = async () => {
   try {
     const data = await fs.promises.readFile('./messages.json', 'utf-8');
-    messages = JSON.parse(data); 
-  } catch (error) { 
+    messages = JSON.parse(data);
+  } catch (error) {
     if (error.code === 'ENOENT') {
       console.log('No se encontró messages.json, se crea uno nuevo.');
       messages = [];
       await fs.promises.writeFile('./messages.json', JSON.stringify(messages, null, 2));
     } else {
-    console.error('Error al intentar cargar los mensajes previos:', error);}
+      console.error('Error al intentar cargar los mensajes previos:', error);
+    }
   }
 };
 
 loadMessageFromFile();
 
-// Configuración de socket 
-
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-    console.log(`Nuevo cliente conectado con el id ${socket.id}`);
+  console.log(`Nuevo cliente conectado con el id ${socket.id}`);
 
-    socket.on("newUser", (data) => {
-      socket.userName = data;
-      socket.broadcast.emit("newUser", data);
-      saveMessages(messages);
-    });
+  socket.on("newUser", (data) => {
+    socket.userName = data;
+    socket.broadcast.emit("newUser", data);
+    saveMessages(messages);
+  });
 
-    socket.on("message", (data) => {
-      messages.push(data);
-      io.emit("messageLogs", messages);
-      saveMessages(messages);
-    });
+  socket.on("message", (data) => {
+    messages.push(data);
+    io.emit("messageLogs", messages);
+    saveMessages(messages);
+  });
 
-    socket.on("disconnect", () => {
-      if (socket.userName) {
-        io.emit("user-disconnect", socket.userName);
-        console.log(`${socket.userName} se desconectó`)
-      }
-    })
-})
-
+  socket.on("disconnect", () => {
+    if (socket.userName) {
+      io.emit("user-disconnect", socket.userName);
+      console.log(`${socket.userName} se desconectó`);
+    }
+  });
+});
